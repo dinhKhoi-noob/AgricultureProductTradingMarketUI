76c1ce703b24b35ae8705151ce18950126d17d15
dinhKhoi-noob <khoip1305@gmail.com>
2022-01-19 20:48:47 +0700

    feat(buying-request-page): Building mpdal for buying request page

--- src/components/request/BuyingRequestCard.tsx
+++ src/components/request/BuyingRequestCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, CircularProgress, Container, Grid, LinearProgress, Tooltip, Typography } from '@mui/material';
+import { Box, Button, CircularProgress, Container, Grid, LinearProgress, Modal, Tooltip, Typography } from '@mui/material';
 import React, { useContext, useEffect, useState } from 'react';
 import Image from 'next/image';
 import NoUserImage from '../../../public/assets/no_user.jpg';
@@ -32,8 +32,14 @@ const BuyingRequestCard = (props: BuyingRequestCardProps) => {
     const quantityRatio = (progress / quantity) * 100;
     const {xsMatched,mdMatched,smMatched} = useContext(LayoutContext);
     const [fontSize,setFontSize] = useState(16);
+    const [isOnpendModal,setIsOpenedModal] = useState(false);
+    
+    const toggleOpenModal = (status: boolean) => {
+        setIsOpenedModal(status);
+    }
 
     useEffect(()=>{
+        console.log(mdMatched,smMatched,xsMatched);
         if(mdMatched){
             setFontSize(16);
             return;
@@ -49,47 +55,69 @@ const BuyingRequestCard = (props: BuyingRequestCardProps) => {
         return ()=>{
             setFontSize(16);
         }
-    },[xsMatched,smMatched,mdMatched])
+    },[xsMatched,smMatched])
     
     return (
-        <Grid container justifyContent="space-between" alignItems="center" mb={1} mt={1}>
-            <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
-                <Image src={NoUserImage} width={80} height={80}></Image>
-            </Grid>
-            <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
-                <Typography fontSize={fontSize}>
-                    {title}
-                </Typography>
-            </Grid>
-            <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
-                <Box display="flex">
-                    <Tooltip title={<Image src={NoUserImage} width={30} height={30}/>}>
-                        <Typography fontSize={fontSize}>
-                            {postBy.username}
-                        </Typography>
-                    </Tooltip>
+        <>
+            <Modal
+                open={isOnpendModal}
+                onClose={()=>{
+                    toggleOpenModal(false)
+                }}
+                aria-labelledby="unstyled-modal-title"
+                aria-describedby="unstyled-modal-description"
+            >
+                <Box width={200} height={200} position="absolute" left="50%" top="50%" style={{backgroundColor:"white",transform:"translate(-50%,-50%)"}}>
+                    <Box display="flex">
+                        <Button variant='outlined' color="success">Đăng ký</Button>
+                        <Button variant='outlined' color="warning">Hủy</Button>
+                    </Box>
                 </Box>
+            </Modal>
+            <Grid container justifyContent="space-between" alignItems="center" mb={1} mt={1}>
+                <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
+                    <Image src={NoUserImage} width={80} height={80}></Image>
+                </Grid>
+                <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
+                    <Typography fontSize={fontSize}>
+                        {title}
+                    </Typography>
+                </Grid>
+                <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
+                    <Box display="flex">
+                        <Tooltip title={<Image src={NoUserImage} width={30} height={30}/>}>
+                            <Typography fontSize={fontSize}>
+                                {postBy.username}
+                            </Typography>
+                        </Tooltip>
+                    </Box>
+                </Grid>
+                <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
+                    <Typography fontSize={fontSize}>
+                        {price}
+                    </Typography>
+                </Grid>
+                <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+                    <Typography>
+                        {progress}/{quantity}
+                    </Typography>
+                    <React.Fragment>
+                        <LinearProgress variant="determinate" value={quantityRatio} style={{width:"80%"}}/>
+                    </React.Fragment>
+                </Grid>
+                <Grid item xs={12} sm={12} md={2} lg={2} xl={2}>
+                    <Box mb={2}/>
+                    <Button
+                        variant='contained'
+                        color="secondary"
+                        fullWidth={xsMatched?true:false} style={{fontSize}}
+                        onClick={()=>{toggleOpenModal(true)}}
+                    >
+                        Đăng ký mua
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid item xs={0} sm={0} md={2} lg={2} xl={2}>
-                <Typography fontSize={fontSize}>
-                    {price}
-                </Typography>
-            </Grid>
-            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
-                <Typography>
-                    {progress}/{quantity}
-                </Typography>
-                <React.Fragment>
-                    <LinearProgress variant="determinate" value={quantityRatio} style={{width:"80%"}}/>
-                </React.Fragment>
-            </Grid>
-            <Grid item xs={12} sm={12} md={2} lg={2} xl={2}>
-                <Box mb={2}/>
-                <Button variant='contained' color="secondary" fullWidth={xsMatched?true:false} style={{fontSize}}>
-                    Đăng ký mua
-                </Button>
-            </Grid>
-        </Grid>
+        </>
     )
 }
 
